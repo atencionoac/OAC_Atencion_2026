876c24d0086e9638f2c9c1a9b46ba2dfee811dd9
atencionoac <atencionoac@gmail.com>
2026-05-28 16:32:41 -0400

    Update proyectos.py

--- proyectos.py
+++ proyectos.py
@@ -12,8 +12,7 @@ st.set_page_config(
 )
 
 # 3. Función de carga de datos
-# @st.cache_data()
-@st.cache_resource
+@st.cache_data()
 def cargar_y_unificar_datos():
     try:
         # 3. Leemos los archivos usando las rutas absolutas corregidas
