2e2d1482faa710c1cf8aff4e0b275e4bddaf0521
atencionoac <atencionoac@gmail.com>
2026-05-28 15:52:55 -0400

    Update proyectos.py

--- proyectos.py
+++ proyectos.py
@@ -12,7 +12,7 @@ st.set_page_config(
 )
 
 # 3. Función de carga de datos
-@st.cache_data(show_spinner=False)
+# @st.cache_data(show_spinner=False)
 @st.cache_resource
 def cargar_y_unificar_datos():
     try:
